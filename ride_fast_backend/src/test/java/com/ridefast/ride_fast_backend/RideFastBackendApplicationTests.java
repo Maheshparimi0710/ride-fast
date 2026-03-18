4a9a9368ff989fe89cfe903e3b96a9198b600a2b
mahesh parimi <maheshparimi83@gmail.com>
2026-03-18 18:35:11 +0530

    Initial commit

---
+++ ride_fast_backend/src/test/java/com/ridefast/ride_fast_backend/RideFastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridefast.ride_fast_backend;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+class RideFastBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
